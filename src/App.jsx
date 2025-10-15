ab27c9ed7097a6f64f7b21c8acc56994c1326f87
Swapon Biswas <swaponbiswas700@gmail.com>
2025-10-15 19:13:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./app.css";
+
+import ProductTab from"./ProductTab.jsx";
+
+
+
+
+function App() {
+ return(
+    <>
+    <h2>Blockbuster Deals | Shop Now</h2>
+    <ProductTab/>
+    </>
+  
+ );
+
+
+
+  
+
+}
+
+export default App
